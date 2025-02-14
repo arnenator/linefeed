efc7d0f21eb7cbbe683352e57857e4faf4612a1c
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-02-14 08:06:35 +0100

    Register the semantic token server capabilities

--- lsp/src/main.rs
+++ lsp/src/main.rs
@@ -10,7 +10,35 @@ struct Backend {
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
     async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
-        Ok(InitializeResult::default())
+        Ok(InitializeResult {
+            server_info: None,
+            capabilities: ServerCapabilities {
+                semantic_tokens_provider: Some(
+                    // TODO: This looks like crap... provide some kind of builder pattern instead
+                    SemanticTokensServerCapabilities::SemanticTokensOptions(
+                        SemanticTokensOptions {
+                            work_done_progress_options: WorkDoneProgressOptions {
+                                work_done_progress: Some(false),
+                            },
+                            legend: SemanticTokensLegend {
+                                token_types: vec![
+                                    SemanticTokenType::NUMBER,   // 0
+                                    SemanticTokenType::STRING,   // 1
+                                    SemanticTokenType::REGEXP,   // 2
+                                    SemanticTokenType::OPERATOR, // 3
+                                    SemanticTokenType::KEYWORD,  // 4
+                                    SemanticTokenType::VARIABLE, // 5
+                                ],
+                                token_modifiers: vec![],
+                            },
+                            range: None,
+                            full: Some(SemanticTokensFullOptions::Bool(true)),
+                        },
+                    ),
+                ),
+                ..Default::default()
+            },
+        })
     }
 
     async fn initialized(&self, _: InitializedParams) {
